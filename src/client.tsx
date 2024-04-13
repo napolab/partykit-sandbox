8e503fa1d9da4396fec432f485140af379e5d2a4
naporin0624 <naporin0624@gmail.com>
2024-04-13 21:42:57 +0000

    chore: reaction counter を作った

---
+++ src/client.tsx
@@ -0,0 +1,100 @@
+import usePartySocket from "partysocket/react";
+import { type FC, useCallback, useEffect, useMemo, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { parse } from "valibot";
+import { MessageType, type Reaction } from "./server";
+
+import "./styles.css";
+
+type RoomProps = {
+	id: string;
+};
+const Root: FC<RoomProps> = ({ id }) => {
+	const [reactions, setReactions] = useState<Reaction[]>([]);
+	const socket = usePartySocket({
+		room: id,
+		onMessage(message) {
+			if (typeof message.data !== "string") return;
+
+			try {
+				const msg = parse(MessageType, JSON.parse(message.data));
+				switch (msg.type) {
+					case "connect":
+						setReactions(msg.payload.reactions);
+						break;
+					case "update:reaction":
+						setReactions(msg.reactions);
+						break;
+					default:
+						console.log("Unknown message type", msg);
+						break;
+				}
+			} catch (e) {
+				console.error(e);
+			}
+		},
+	});
+
+	const decrement = useCallback(
+		(name: string) => {
+			const msg: MessageType = { type: "decrement", name: name };
+			socket.send(JSON.stringify(msg));
+		},
+		[socket],
+	);
+	const increment = useCallback(
+		(name: string) => {
+			const msg: MessageType = { type: "increment", name: name };
+			socket.send(JSON.stringify(msg));
+		},
+		[socket],
+	);
+
+	return (
+		<section>
+			<h2>Room: {id}</h2>
+
+			<ul className="reaction-list">
+				{reactions.map((reaction) => (
+					<li key={reaction.name} className="reactions-list__item">
+						<button type="button" onClick={() => decrement(reaction.name)} className="decrement">
+							-
+						</button>
+
+						<span className="emoji">
+							<span>{reaction.name}</span>
+							<span>{`${reaction.count}`.padStart(2, "0")}</span>
+						</span>
+
+						<button type="button" onClick={() => increment(reaction.name)} className="increment">
+							+
+						</button>
+					</li>
+				))}
+			</ul>
+		</section>
+	);
+};
+
+const ROOM_ID_KEY = "id";
+const App = () => {
+	const url = useMemo(() => new URL(location.href), []);
+	const [room] = useState(url.searchParams.get(ROOM_ID_KEY) ?? "room");
+
+	useEffect(() => {
+		url.searchParams.set(ROOM_ID_KEY, room);
+		history.replaceState(null, "", url.toString());
+	}, [room, url]);
+
+	return (
+		<div className="root">
+			<h1>PartyKit Demo</h1>
+			<Root id={room} />
+		</div>
+	);
+};
+
+const app = document.getElementById("app");
+if (app) {
+	createRoot(app).render(<App />);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,38 @@
+import { get, has } from "node-emoji";
 import type * as Party from "partykit/server";
+import { type Output, array, custom, literal, number, object, parse, string, union } from "valibot";
+
+export const Reaction = object({
+	name: string([custom((value) => has(value))]),
+	count: number(),
+});
+export type Reaction = Output<typeof Reaction>;
+
+export const MessageType = union([
+	object({
+		type: literal("connect"),
+		payload: object({
+			reactions: array(Reaction),
+		}),
+	}),
+	object({
+		type: literal("update:reaction"),
+		reactions: array(Reaction),
+	}),
+	object({
+		type: literal("increment"),
+		name: string(),
+	}),
+	object({
+		type: literal("decrement"),
+		name: string(),
+	}),
+]);
+export type MessageType = Output<typeof MessageType>;
 
 export default class Server implements Party.Server {
 	readonly options: Party.ServerOptions = { hibernate: true };
+	private reactions = new Map<Reaction["name"], Reaction>();
 
 	constructor(readonly room: Party.Room) {
 		//
@@ -23,21 +54,74 @@ export default class Server implements Party.Server {
 	}
 
 	async onStart() {
-		console.log("start", await this.room.storage.list());
+		const reactions = await this.room.storage.get<Map<Reaction["name"], Reaction>>("reactions");
+		const emojis = [get("+1"), get("balloon"), get("heart")].filter((v): v is string => v !== undefined && v !== "");
+
+		this.reactions = reactions ?? new Map(emojis.map((emoji) => [emoji, { name: emoji, count: 0 }]));
+		if (reactions === undefined) {
+			this.room.storage.put("state:reactions", this.reactions);
+		}
 	}
 
 	async onRequest() {
-		return Response.json({ message: "Hello, world!" });
+		return Response.json({ reactions: Array.from(this.reactions.values()) });
 	}
 
 	async onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-		console.log(`Connected: id: ${conn.id} room: ${this.room.id} url: ${new URL(ctx.request.url).pathname}`);
-		console.log("connect", ctx.request.headers.get("x-before-connect"));
-		conn.send("hello from server");
+		const msg: MessageType = {
+			type: "connect",
+			payload: { reactions: Array.from(this.reactions.values()) },
+		};
+		conn.send(JSON.stringify(msg));
 	}
 
-	async onMessage(message: string, sender: Party.Connection) {
-		this.room.broadcast(`${sender.id}: ${message}`, [sender.id]);
+	async onMessage(message: string) {
+		try {
+			const msg: MessageType = parse(MessageType, JSON.parse(message));
+
+			switch (msg.type) {
+				case "connect":
+					break;
+				case "increment": {
+					let reaction = this.reactions.get(msg.name);
+					if (reaction === undefined) {
+						reaction = { name: msg.name, count: 1 };
+						this.reactions.set(msg.name, reaction);
+					} else {
+						reaction.count = Math.min(reaction.count + 1, 99);
+						this.reactions.set(msg.name, reaction);
+					}
+					this.room.storage.put("state:reactions", this.reactions);
+
+					const notify: MessageType = {
+						type: "update:reaction",
+						reactions: Array.from(this.reactions.values()),
+					};
+					this.room.broadcast(JSON.stringify(notify));
+					break;
+				}
+				case "decrement": {
+					let reaction = this.reactions.get(msg.name);
+					if (reaction === undefined) {
+						reaction = { name: msg.name, count: 0 };
+						this.reactions.set(msg.name, reaction);
+					} else {
+						reaction.count = Math.max(0, reaction.count - 1);
+						this.reactions.set(msg.name, reaction);
+					}
+					this.room.storage.put("state:reactions", this.reactions);
+
+					const notify: MessageType = {
+						type: "update:reaction",
+						reactions: Array.from(this.reactions.values()),
+					};
+					this.room.broadcast(JSON.stringify(notify));
+					break;
+				}
+			}
+		} catch (e) {
+			console.error(e);
+		}
 	}
 }
 
